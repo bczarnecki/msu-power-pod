78f80c7a05e8d6406a4959edf480a7825a09820d
Brendan Czarnecki <czarnecki.brendan@gmail.com>
2019-03-13 21:00:06 -0400

    Thought I committed/pushed these already but I somehow didnt.
    lcd.ino:
      - added block char to make bar on LCD to display as a percentage of 
    battery life left
      - added dummy code to test LCD screen

--- main/lcd.ino
+++ main/lcd.ino
@@ -2,12 +2,24 @@
 
 LiquidCrystal LCD(8, 9, 4, 5, 6, 7); // Pin order matters
 const int backlight = 10;
+const byte blockChar = 0;
+const byte block[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+};
 
 void LCDinit(){
     /*
         Initializes LCD screen and sets cursor to 0,0
     */
     LCD.begin(16, 2); // 16 columns, 2 rows
+    LCD.createChar(0, block); // create block character
     LCD.setCursor(0,0);
     pinMode(backlight, OUTPUT); // pin 10 is LCD backlight
     backlightOn();
@@ -17,6 +29,15 @@ void refreshScreen(){
     /*
         Updates the LCD screen with current information
     */
+
+    /*** Dummy screen code for testing ***/
+    LCD.print("Time: 14");
+    LCD.setCursor(0,1);
+    for(int i = 0; i < 11; i++){
+        LCD.write(blockChar);
+    }
+    /*** End of dummy screen code ***/
+
     LCD.setCursor(0,0); // Always set back to origin when done
 }
 
