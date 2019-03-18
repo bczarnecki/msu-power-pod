2a770411b006a385388fd86d76717162f8867395
Panashe Mayangamutse <mayangam@msu.edu>
2019-03-18 11:37:05 -0400

    Added the sound function to activate and deactivate alarm sound

--- main/sound.ino
+++ main/sound.ino
@@ -1,1 +1,30 @@
+/*
+ * Simple activate and deactivate alarm function 
+ * TODO 
+ *Create a range of tones for different durations
+ */
 
+//Placeholder Pin
+const int soundPin = 11;
+
+void soundInit(){
+  
+  pinMode(soundPin,OUTPUT);
+  digitalWrite(soundPin, LOW);
+  
+}
+
+void activateAlarm(){
+  
+  //tone(pin,frequency,duration in milliseconds)
+  digitalWrite(soundPin, HIGH);
+  tone(soundPin, 2000, 500);
+  
+}
+
+void deactivateAlarm(){
+  
+  digitalWrite(soundPin, LOW);
+  noTone(soundPin);
+
+}
